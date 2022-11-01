c509cdec49aecde968ae25ba931baa36b47a1c5b
taehee <taehee.jang@gitple.com>
2022-11-01 12:35:02 +0900

    Fix v0.0.2 - type

--- index.d.ts
+++ index.d.ts
@@ -1,18 +1,31 @@
 import EventEmitter from 'events';
+export interface ClientError {
+    code: number;
+    name: string;
+    message: string;
+    stack: string;
+}
 export interface InitConfig {
     user_id: string;
     token: string;
+    debug?: boolean;
 }
 export declare class VoiceClient extends EventEmitter {
     private user_id;
     private token;
+    private debug;
     private nexmoClient;
     private session;
     private currentCall;
     constructor(config: InitConfig);
     connectUser(): Promise<void>;
-    createSession(): Promise<void>;
+    disconnectUser(): Promise<void>;
     call(to: string): Promise<void>;
     hangUp(): Promise<void>;
     dialUp(): Promise<void>;
+    private createSession;
+    private deleteSession;
+    private destroyCall;
+    private clearSession;
+    private clearCall;
 }
